5887c284ad2eb53a3ad42dc08d3d04c8da467b3f
maikelmclauflin <michael.j.mclaughlin20@gmail.com>
2020-01-29 02:21:05 -0600

    Updates

---
+++ precache-manifest.93416e544d16b68ff91241877163f492.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b617775ac8707658523718d2423d618",
+    "url": "./index.html"
+  },
+  {
+    "revision": "8935b040eccb2488c922",
+    "url": "./static/css/main.4283d02f.chunk.css"
+  },
+  {
+    "revision": "911a04bb05a8ecd389f6",
+    "url": "./static/js/2.c00856cf.chunk.js"
+  },
+  {
+    "revision": "8935b040eccb2488c922",
+    "url": "./static/js/main.83ae9137.chunk.js"
+  },
+  {
+    "revision": "dd22929369d137db1959",
+    "url": "./static/js/runtime-main.dbd7deed.js"
+  }
+]);
